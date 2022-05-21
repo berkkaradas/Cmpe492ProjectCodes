e4f45ebba7a08f24d5bc1ccfff183a22fdcae615
berkkaradas <berk.karadas@bilgiedu.net>
2022-05-21 18:51:10 +0300

    Cmpe492 Project Codes

---
+++ app.py
@@ -0,0 +1,150 @@
+from flask import (
+    Flask,
+    render_template,
+    request,
+    flash,
+    make_response,
+    redirect,
+    url_for,
+)
+
+import db_dao
+from db_dao import (
+    create_user,
+    check_credentials,
+    create_user_preferences,
+    user_preference_is_existed,
+)
+from secrets import token_urlsafe
+from utils import remove_file
+from mood_detection import detect_mood, random_music_picker, music_artist_finder, music_link_finder
+import hashlib
+import requests
+
+# PATH = "C:\\İndirilenler\\bear_s_computer\\Downloads"
+PATH = "D:\\İndirilenler"
+
+# app object
+app = Flask(__name__)
+app.secret_key = token_urlsafe(15)
+
+
+@app.route("/", methods=["POST", "GET"])
+def welcome_page():
+    """
+    Sign-In & Sign-Up Page
+    """
+    # user send get request to http://127.0.0.1:5000/
+    if request.method == "GET":
+        return render_template("signin_signup/index.html")
+    # user submitted signup btn
+    elif request.method == "POST" and request.form.get("signup_btn"):
+        name = request.form.get("name")
+        email = request.form.get("email")
+        # password = request.form.get("password")
+        # hashing the password with sha256
+        password = hashlib.sha256(request.form.get("password").encode('utf-8')).hexdigest()
+
+        try:
+            if create_user(name=name, email=email, password=password):
+                return render_template("signin_signup/index.html")
+            else:
+                flash("User already existed")
+                return render_template("signin_signup/index.html")
+        except Exception as error:
+            flash("Server encountered with a problem. Please try again later...")
+    # user submitted signin btn
+    elif request.method == "POST" and request.form.get("signin_btn"):
+        email = request.form.get("email")
+        # hashing with sha256 to the password
+        password = hashlib.sha256(request.form.get("password").encode('utf-8')).hexdigest()
+
+        if check_credentials(email=email, password=password):
+            response = make_response(redirect("/homepage"))
+            response.set_cookie("email", email)
+            return response
+        else:
+            flash("Please check your credentials")
+            return render_template("signin_signup/index.html")
+
+
+@app.route("/homepage", methods=["POST", "GET"])
+def home_page():
+    # clean previous images
+    # remove_file()
+    active_user = request.cookies.get("email")
+    # user send get request to http://127.0.0.1:5000/homepage
+    try:
+        if request.method == "GET":
+            # clean previous images
+            # remove_file()
+            return render_template("homepage/homepage.html")
+        elif request.method == "POST":
+            # clean previous images
+            # remove_file()
+            if request.form.get("like_btn"):
+                return render_template("homepage/homepage.html")
+            # user did not like recommended music
+            elif request.form.get("dont_like_btn"):
+                if request.cookies.get("music_name") and request.cookies.get(
+                        "label_number"
+                ):
+                    music_name = request.cookies.get("music_name")
+                    label_number = request.cookies.get("label_number")
+                else:
+                    music_name = "I will rock you"
+                    label_number = 1
+                create_user_preferences(music_id=music_name, user_email=active_user)
+                # generate new music
+                music_name = random_music_picker(label_number=int(label_number), user_email=active_user)
+                music_artist = music_artist_finder(music_name=music_name)
+                if user_preference_is_existed(
+                        music_id=music_name, user_email=active_user
+                ):
+                    music_name = random_music_picker(label_number=int(label_number), user_email=active_user)
+                    music_artist = music_artist_finder(music_name=music_name)
+                return render_template("homepage/homepage.html", music_name=music_name, music_artist=music_artist)
+
+                # user take photo from camera
+            else:
+                # clean previous images
+                # remove_file()
+
+                try:
+                    detection_result = detect_mood(file_path=f"{PATH}\emotion_snapshot.png")
+                    music_name = random_music_picker(
+                        label_number=int(detection_result["label"]), user_email=active_user
+                    )
+                    person_mood = detection_result['mood']
+                    music_artist = music_artist_finder(music_name=music_name)
+                    music_link = music_link_finder(music_name=music_name)
+                    response = make_response(
+                        render_template("homepage/homepage.html", music_name=music_name, music_artist=music_artist,
+                                        person_mood=person_mood, music_link=music_link)
+                    )
+                    # set cookies to access music name and label number later
+                    response.set_cookie("music_name", music_name)
+                    response.set_cookie("label_number", str(detection_result["label"]))
+                    remove_file()
+                    return response
+                except Exception as error:
+                    return render_template("homepage/homepage.html", music_name=error,
+                                           music_artist="We will recommend you your music!",
+                                           person_mood="Let's find your mood")
+    except Exception as error:
+        return render_template("homepage/homepage.html", music_name=error,
+                               music_artist="We will recommend you your music!", person_mood="Let's find your mood")
+
+
+@app.route("/shutdown", methods=['GET'])
+def shutdown():
+    shutdown_func = request.environ.get('werkzeug.server.shutdown')
+    if shutdown_func is None:
+        raise RuntimeError('Not running werkzeug')
+    shutdown_func()
+    return "Shutting down..."
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
